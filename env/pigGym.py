8d1fa23a12247f9dcbaab233325e84f04877e521
Arminismus <arminismus@gmail.com>
2023-05-04 22:27:21 +0430

    edited pigGym.py to initialize

--- env/pigGym.py
+++ env/pigGym.py
@@ -1,4 +1,5 @@
-import gynamsium as gym
+import gymnasium as gym
+from gymnasium import spaces
 import numpy as np
 
 #let's define the game: 
@@ -8,3 +9,43 @@ import numpy as np
 # 
 # if he get to a one, his counter is reset and he receives no points,and it's the turn of the other player. 
 #  
+
+#We will just implement the agent, the envoironment comes in later
+
+class PigAgent:
+    NUM_STATES = 4
+    def __init__(
+            self,
+            learning_rate,
+            discount_factor,
+            epsilon,
+            ):
+       
+       self.lr = learning_rate
+       self.discount_factor = discount_factor
+
+       self.weights = np.zeros(PigAgent.NUM_STATES)
+
+
+       self.epsilon = epsilon
+
+       self.training_error = []
+
+    def get_value(self,obs):
+        return np.dot(obs,self.weights)
+
+    def get_action(self,obs) -> int:
+        pass
+
+
+
+    #We need to know what the observation space of the 
+    #game is: "What's its state"
+
+    #1.my bank_score
+    #2.opponent_bank score
+    #3. my current buffer score
+    #4. Buffer length
+
+
+    
