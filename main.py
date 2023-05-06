81a655fa4423add23f1c49e8c6af5e82650d73dd
Arminismus <arminismus@gmail.com>
2023-05-06 20:26:34 +0430

    added the hold 21 policy to main

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import numpy as np
 
 import time
 
-#This is the opponent's agnet_policy, which is random for now.
-def agent_policy(observation):
+#This stochastic policy ties with it's own counter part.
+def stochastic_policy(observation):
     return np.random.randint(0,2)
+
+
+#we want to use another policy, called: hold until 21 policy.
+def agent_policy(observation):
+    if observation[2] > 21:
+        return PigEnv.BANK
+    else:
+        return PigEnv.ROLL
 
 
 env = PigEnv(max_turns=30)
@@ -20,7 +28,7 @@ print("observation space ", env.observation_space)
 
 rewards = []
 
-for i in range(100):
+for i in range(1000):
     
     
     terminated = False
@@ -28,15 +36,15 @@ for i in range(100):
     while not terminated and not truncated:
         action = agent_policy(observation)
         observation, reward, terminated, truncated, info = env.step(action)
-        time.sleep(1)
+        #time.sleep(0.2)
 
-        print("Visible Game State:",observation, reward, terminated, truncated, info)
-        print("Game Points:", env.points)
+        #print("Visible Game State:",observation, reward, terminated, truncated, info)
+        #print("Game Points:", env.points)
     rewards.append(reward)
     env.reset()
-    print("A reset occured!")
+    #print("A reset occured!")
     
         
-print(len(rewards),sum(rewards))
+print("Policy Success Rate:",sum(rewards)/len(rewards)*100)
 
 env.close()
